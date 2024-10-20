deeb071057231ea161a97494e27f6e82ae243de8
alain <alaynfm@chucks.local>
2024-10-20 21:27:10 +0200

    Test external

--- sarifToSonar.py
+++ sarifToSonar.py
@@ -1,58 +1,35 @@
 import json
 
-def convert_to_sarif(sarif_file, output_file, engine_id):
+def convert_to_sonar_format(sarif_file, output_file, engine_id):
     with open(sarif_file, 'r') as f:
         sarif_data = json.load(f)
 
-    sarif_output = {
-        "version": "2.1.0",
-        "$schema": "http://json.schemastore.org/sarif-2.1.0-rtm.5",
-        "runs": []
-    }
-
-    runs = {
-        "tool": {
-            "driver": {
-                "name": engine_id,  # Engine name (e.g., Checkov, TFSec)
-                "informationUri": "https://www.test-linter-url.com",  # Can be customized
-                "version": "1.0.0"  # Version of the tool (customize as needed)
-            }
-        },
-        "results": []
+    sonar_output = {
+        "issues": []
     }
 
     for run in sarif_data.get("runs", []):
         for result in run.get("results", []):
             for location in result.get("locations", []):
-                result_data = {
-                    "level": result.get("level", "warning"),  # Error level
-                    "message": {
-                        "text": result.get("message", {}).get("text", "")
-                    },
-                    "locations": [
-                        {
-                            "physicalLocation": {
-                                "artifactLocation": {
-                                    "uri": location.get("physicalLocation", {}).get("artifactLocation", {}).get("uri", "")
-                                },
-                                "region": {
-                                    "startLine": location.get("physicalLocation", {}).get("region", {}).get("startLine", 1),
-                                    "startColumn": location.get("physicalLocation", {}).get("region", {}).get("startColumn", 1),
-                                    "endLine": location.get("physicalLocation", {}).get("region", {}).get("endLine", 1),
-                                    "endColumn": location.get("physicalLocation", {}).get("region", {}).get("endColumn", 1)
-                                }
-                            }
+                issue_data = {
+                    "engineId": engine_id,  # Engine name (e.g., Checkov, TFSec)
+                    "ruleId": result.get("ruleId", ""),
+                    "severity": result.get("level", "MAJOR").upper(),  # Convert SARIF levels to SonarQube severity
+                    "type": "VULNERABILITY",  # Can be customized as needed
+                    "primaryLocation": {
+                        "message": result.get("message", {}).get("text", ""),
+                        "filePath": location.get("physicalLocation", {}).get("artifactLocation", {}).get("uri", ""),
+                        "textRange": {
+                            "startLine": location.get("physicalLocation", {}).get("region", {}).get("startLine", 1),
+                            "endLine": location.get("physicalLocation", {}).get("region", {}).get("endLine", 1)
                         }
-                    ],
-                    "ruleId": result.get("ruleId", "")
+                    }
                 }
-                runs["results"].append(result_data)
-    
-    sarif_output["runs"].append(runs)
+                sonar_output["issues"].append(issue_data)
 
     with open(output_file, 'w') as f:
-        json.dump(sarif_output, f, indent=2)
+        json.dump(sonar_output, f, indent=2)
 
-# Convert Checkov and TFSec SARIF files to SonarQube format
-convert_to_sarif('results_sarif.sarif', 'checkov-sonarqube.json', 'checkov')
-convert_to_sarif('tfsec.sarif', 'tfsec-sonarqube.json', 'tfsec')
+# Convert Checkov and TFSec SARIF files to SonarQube-compatible format
+convert_to_sonar_format('results_sarif.sarif', 'checkov-sonarqube.json', 'checkov')
+convert_to_sonar_format('tfsec.sarif', 'tfsec-sonarqube.json', 'tfsec')
